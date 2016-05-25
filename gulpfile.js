281dc7da0594cc59686983efafc444baf67c575a
Tony Brown <tony@tonybrown.me>
2016-05-24 22:10:50 -0400

    setup browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,36 @@
 'use strict';
 
-var gulp    = require('gulp');
-var connect = require('gulp-connect');// runs local dev server
-var open    = require('gulp-open'); // open URL in a web browser
+var gulp       = require('gulp');
+var connect    = require('gulp-connect');// runs local dev server
+var open       = require('gulp-open'); // open URL in a web browser
+var browserify = require('browserify'); // bundle our js
+var reactify   = require('reactify'); // Transforms react jsx into js
+var source     = require('vinyl-source-stream'); //use conventional text streams with gulp
 
 var config = {
 	port: 9005,
 	devBaseUrl: 'http://localhost',
 	paths: {
-		html: './src/*.html',
-		dist: './dist'
+		html : './src/*.html',
+		js   : './src/**/*.js',
+		dist : './dist',
+		mainJs: './src/main.js'
 	}
 }
 
 // start a local dev server
 gulp.task('connect', function () {
 	connect.server({
-		root: ['dist'],
-		port: config.port,
-		base: config.devBaseUrl,
-		livereload: true
+		root       : ['dist'],
+		port       : config.port,
+		base       : config.devBaseUrl,
+		livereload : true
 	});
 });
 
 gulp.task('open',['connect'], function () {
 	gulp.src('dist/index.html')
-			.pipe(open({ uri: config.devBaseUrl + ':' + config.port + '/' }));
+			.pipe(open('', { url: config.devBaseUrl + ':' + config.port + '/' }));
 });
 
 gulp.task('html', function () {
@@ -34,8 +39,19 @@ gulp.task('html', function () {
 			.pipe(connect.reload());
 });
 
+gulp.task('js', function () {
+	browserify(config.paths.mainJs)
+		.transform(reactify)
+		.bundle()
+		.on('error', console.error.bind(console))
+		.pipe(source('bundle.js'))
+		.pipe(gulp.dest(config.paths.dist + '/scripts'))
+		.pipe(connect.reload());
+})
+
 gulp.task('watch', function() {
 	gulp.watch(config.paths.html, ['html']);
+	gulp.watch(config.paths.js,['js']);
 });
 
-gulp.task('default', ['html', 'open', 'watch']);
+gulp.task('default', ['html', 'js', 'open', 'watch']);
--- src/main.js
+++ src/main.js
@@ -1,5 +1,1 @@
-$ = jQuery = require('jquery')
-var React = require('react');
-var Home = require('./components/homePage');
-
-React.render(<Home />, document.getElementById('app'));
+var App = console.log('Hello world from Browserify!!!!');
